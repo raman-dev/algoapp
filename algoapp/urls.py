4bbb381256a819e85ed4a9eb0cbaf27ac973602c
Raman <ramandeep.de582@gmail.com>
2020-07-17 12:01:44 -0400

    added d3 node drawing in svgs with transitions and removed canvas drawing; removed on hover dropdown, returned to onclick behaviour

--- algoapp/urls.py
+++ algoapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path('',views.index,name='index'),
+    re_path(r'^test3d/$',views.test_d3,name='test3d')
 ]
--- algoapp/views.py
+++ algoapp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'algoapp/index.html')
+
+def test_d3(request):
+    return render(request,'algoapp/testd3.html')
